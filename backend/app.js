264d77e1d254cc1f652cfa77aebafb3e90076ee4
Bhushan Bhuskade <119596549+bhuskade1999@users.noreply.github.com>
2023-04-16 16:33:25 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
  const express = require("express")
  const app = express()
  const cookieParser= require("cookie-parser")
+ const path = require("path");
 
 
  if(process.env.NODE_ENV !== 'production') {   
